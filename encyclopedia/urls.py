52f815cf75115e3906520ff2c57b95bbd508ded3
DylanMeegs <118544095+DylanMeegs@users.noreply.github.com>
2022-11-22 20:41:20 +0100

    Add files via upload
    Wiki Assignment

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search/", views.return_search, name="search"),
+    path("new/", views.create, name="create"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit/", views.save_edit, name="save_edit"),
+    path("random/", views.randomize, name="randomize")
 ]
---
+++ entries/Test.md
@@ -0,0 +1,3 @@
+# Test
+
+Dit is een test
